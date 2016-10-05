a13e2090f09f83dea5e87d98c93a72d526612d7a
blugavere <b.lugavere@gmail.com>
2016-10-05 08:33:20 -0400

    fs crud ops

--- test/new.js
+++ test/new.js
@@ -1,2 +1,13 @@
-var log = require('./test')
-log();
+
+
+const Repo = require('../lib/FSRepository');
+const Serializer = require('../lib/Serializers/JSONSerializer');
+const serializer = new Serializer();
+const repo = new Repo(serializer, './data.json');
+
+const obj = { name : 'Delifus' };
+
+repo.add(obj, (err, res) => {
+  console.log('result', err, res)
+});
+//console.log(repo); \
