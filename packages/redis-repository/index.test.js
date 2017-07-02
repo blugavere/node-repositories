3e3a6b55e77755188895d43e9cf717929aff376f
blugavere <b.lugavere@gmail.com>
2017-07-02 12:58:38 -0400

    add expiry testing

--- packages/redis-repository/index.test.js
+++ packages/redis-repository/index.test.js
@@ -1,6 +1,6 @@
-
 'use strict';
 
+const expect = require('expect');
 const redis = require('redis');
 const Repo = require('.');
 const Assertions = require('../../test/assertions');
@@ -26,6 +26,38 @@ describe('Redis Repository', () => {
     Assertions.assertions.forEach(x => {
       it(x.assertion, done => {
         x.method(repo, bag)(done);
+      });
+    });
+  });
+  describe('redis features', () => {
+
+    it('should be able to add with your own id', done => {
+      repo.add({
+        _id: 'x',
+        name: 'y'
+      }, (err, res) => {
+        expect(res._id).toEqual('x');
+        repo.findOne('x', (err, res) => {
+          expect(res.name).toEqual('y');
+          done();
+        });
+      });
+    });
+
+    it('should be able to add with an expiry', done => {
+      repo.add({
+        _id: 'x',
+        name: 'y'
+      }, {
+        expire: 1
+      }, (err, res) => {
+        expect(res._id).toEqual('x');
+        setTimeout(() => {
+          repo.findOne('x', (err, res) => {
+            expect(res).toNotExist();
+            done();
+          });
+        }, 1000);
       });
     });
   });
