f9ea7e1040c47ffb7b3029f0ca82993dfd6169f3
Ben Lugavere <b.lugavere@gmail.com>
2017-07-17 16:15:49 -0400

    simplify

--- packages/mixins/domainify.js
+++ packages/mixins/domainify.js
@@ -1,30 +1,26 @@
 
 'use strict';
 
-const _parseAndReconstitute = factory => doc => Promise.resolve(JSON.parse(JSON.stringify(doc)))
-    .then(data => factory.reconstitute(data));
-
-const parseResponse = parseAndReconstitute => blob => {
+const _reconstitute = factory => blob => {
     if (!blob) {
-        return Promise.resolve();
+        return Promise.resolve(null);
     }
 
     if (Array.isArray(blob)) {
-        return Promise.all(blob.map(doc => parseAndReconstitute(doc)));
+        return Promise.all(blob.map(doc => factory.reconstitute(doc)));
     }
 
-    return parseAndReconstitute(blob);
+    return factory.reconstitute(blob);
 };
 
 
 function domainify(repo, factory) {
-    const parseAndReconstitute = _parseAndReconstitute(factory);
-    const parse = parseResponse(parseAndReconstitute);
+    const reconstitute = _reconstitute(factory);
 
     const wrap = methodName => {
         const orig = repo[methodName].bind(repo);
         repo[methodName] = function () {
-            return orig.apply(null, arguments).then(results => parse(results));
+            return orig.apply(null, arguments).then(results => reconstitute(results));
         };
     };
 
